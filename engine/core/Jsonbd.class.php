ac0ac397337cb944949a0b8ac51c1268d2a70d65
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-02-10 17:56:15 +0300

    3

--- engine/core/Jsonbd.class.php
+++ engine/core/Jsonbd.class.php
@@ -13,7 +13,7 @@ class JsonBd
 		$this->prefix = $prefix;
 		if (!is_dir($path)) {
 			if (!mkdir($path)) {
-				$this->log[date("H:i:s")] = [__LINE__, "Не удалось создать папку"];
+				$this->log[] = [__LINE__, "Не удалось создать папку"];
 				return false;
 			}
 		}
@@ -28,7 +28,7 @@ class JsonBd
 	{
 		if (!is_file($this->model)) {
 			if (!file_put_contents($this->model, json_encode($this->shema, 256))) {
-				$this->log[date("H:i:s")] = [__LINE__, "Не удалось создать Information_Shema"];
+				$this->log[] = [__LINE__, "Не удалось создать Information_Shema"];
 				return false;
 			}
 		}
@@ -41,19 +41,18 @@ class JsonBd
 			if (!mkdir($this->path . "/" . $name)) {
 				return false;
 			}
-
 		}
-		if(!array_search($name, $this->shema["databases"]) === false){
+		if (!array_search($name, $this->shema["databases"]) === false) {
 			$this->shema["databases"][] = $name;
 			return $this->shema_save();
 		}
-		$this->log[date("H:i:s")] = [__LINE__, "$name уже существует"];
+		$this->log[] = [__LINE__, "$name уже существует"];
 		return true;
 	}
 	private function shema_save()
 	{
 		if (!file_put_contents($this->model, json_encode($this->shema, 256))) {
-			$this->log[date("H:i:s")] = [__LINE__, "Не удалось сохранить Information_Shema"];
+			$this->log[] = [__LINE__, "Не удалось сохранить Information_Shema"];
 			return false;
 		}
 	}
@@ -64,7 +63,7 @@ class JsonBd
 	 * @param  array $fields = [
 	 * "name"=>""
 	 * "type"=>""
-	 * "deful_valuet"=>""
+	 * "defult_value"=>""
 	 * "index"=>false
 	 * "auto_increment"=>false
 	 * ]
@@ -73,16 +72,65 @@ class JsonBd
 	 */
 	public function createTable($database, $name, $fields)
 	{
+		$Table = [];
 		$args = func_get_args();
 		$database = array_shift($args);
 		$name = array_shift($args);
 		if (array_search($database, $this->shema["databases"]) === false) {
-			$this->log[date("H:i:s")] = [__LINE__, "$database Не существует пробую создать"];
+			$this->log[] = [__LINE__, "$database Не существует пробую создать"];
 			if (!$this->createBd($database)) {
 				return false;
 			}
 		}
 		foreach ($args as $key => $value) {
+			$feild_pattern = [
+				"name" => "",
+				"type" => "",
+				"defult_value" => "",
+				"index" => false,
+				"auto_increment" => false,
+			];
+			if (!isset($value["name"]) or !isset($value["type"])) {
+				$this->log[] = ["не удалось добавить поле $key нет типа или имени"];
+				continue;
+			}
+			$feild_pattern["name"] = $value['name'];
+			switch ($value["type"]) {
+				case 'string':
+					if (!isset($value["defult_value"])) {
+						$feild_pattern["defult_value"] = '';
+					}
+					break;
+				case 'int':
+					if (!isset($value["defult_value"])) {
+						$feild_pattern["defult_value"] = 0;
+					}
+					break;
+				case 'float':
+					if (!isset($value["defult_value"])) {
+						$feild_pattern["defult_value"] = 0.0;
+					}
+					break;
+				case 'bool':
+					if (!isset($value["defult_value"])) {
+						$feild_pattern["defult_value"] = false;
+					}
+					break;
+
+				default:
+					$this->log[] = ["не удалось добавить поле $key неверный тип данных"];
+					continue 2;
+					break;
+			}
+			$feild_pattern["type"] = $value["type"];
+			$feild_pattern["index"] = $value["index"];
+			$feild_pattern["auto_increment"] = $value["auto_increment"];
+			if ($value["index"]) {
+				$Table["index"] = $value['name'];
+			}
+			$Table["fields"][] = $feild_pattern;
 		}
+		$this->shema["databases"][$database][$name] = $Table;
+		return $this->shema_save();
 	}
 }
--- engine/core/test.php
+++ engine/core/test.php
@@ -2,7 +2,24 @@
 include "Jsonbd.class.php";
 ob_start();
 $test = new JsonBd("JsonBd");
-$test->createBd('test2');
+$test->createTable(
+	'test',
+	"td",
+	[
+		"name" => "rtr",
+		"type" => "int",
+		"defult_value" => "",
+		"index" => true,
+		"auto_increment" => true,
+	],
+	[
+		"name" => "jhj",
+		"type" => "string",
+		"defult_value" => "",
+		"index" => false,
+		"auto_increment" => false,
+	]
+);
 $enter = ob_get_contents();
 var_dump($enter);
 var_dump($test);
