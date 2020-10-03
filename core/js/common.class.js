17dbabb227de2434661e63f74d190f5036f49ef2
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-10-03 19:55:55 +0300

    1

--- core/js/common.class.js
+++ core/js/common.class.js
@@ -1,179 +1,167 @@
-'use strict'
+
 
 class CommonClass {
-	#_empty = true
-	#_ElementId = null
+	#_empty = true;
+	#_ElementId = null;
 	editorFields = {
-		name: new EditorFieldsClass({name: 'name'})
-	}
-
+		name: new EditorFieldsClass({ name: "name" }),
+	};
 
 	constructor() {
-		if(arguments.length == 0) {
-			this.#_empty = true
+		if (arguments.length == 0) {
+			this.#_empty = true;
 		} else {
-			this.#_empty = false
+			this.#_empty = false;
 		}
-		this.sortKey = 0
-		this.classKey = this.constructor.name
+		this.sortKey = 0;
+		this.classKey = this.constructor.name;
 	}
 
-
+	/**
+	 * @param  {Object} manual
+	 * @param  {number} key
+	 */
 	setManual(manual, key) {
-		if(manual instanceof ManualClass) {
-			this.manual = manual
-			this.sortKey = key
-			this.ElementId = key
+		if (manual instanceof ManualClass) {
+			this.manual = manual;
+			this.sortKey = key;
+			this.ElementId = key;
 		}
 	}
 
-
-	render() { }
-
-
+	render() {}
+	editorRender(){}
 	prepare() {
-		return true
+		return true;
 	}
 
-
-	success(msg = '', data = {}) {
-		return {success: true, msg: msg, data: data}
+	success(msg = "", data = {}) {
+		return { success: true, msg: msg, data: data };
 	}
 
-
-	failure(msg = '', data = {}) {
-		return {success: false, msg: msg, data: data}
+	failure(msg = "", data = {}) {
+		return { success: false, msg: msg, data: data };
 	}
-
 
 	get empty() {
-		return this.#_empty
+		return this.#_empty;
 	}
-
 
 	set empty(value) {
-		return false
+		return false;
 	}
-
 
 	get ElementId() {
-		return this.#_ElementId
+		return this.#_ElementId;
 	}
-
 
 	set ElementId(value) {
-		if(this.#_ElementId == null) {
-			this.#_ElementId = value
+		if (this.#_ElementId == null) {
+			this.#_ElementId = value;
 		}
 	}
-
 
 	toObject() {
-		var JSON = {}
-		for(let key in this) {
-			let element = this[key]
-			if(element instanceof CommonClass) {
-				JSON[key] = element.toObject()
-				continue
+		var JSON = {};
+		for (let key in this) {
+			let element = this[key];
+			if (element instanceof CommonClass) {
+				JSON[key] = element.toObject();
+				continue;
 			}
-			if(element instanceof Array) {
-				JSON[key] = []
-				for(let k in element) {
-					let e = element[k]
-					if(e instanceof CommonClass) {
-						JSON[key].push(e.toObject())
+			if (element instanceof Array) {
+				JSON[key] = [];
+				for (let k in element) {
+					let e = element[k];
+					if (e instanceof CommonClass) {
+						JSON[key].push(e.toObject());
 					}
 				}
-				continue
+				continue;
 			}
-			JSON[key] = element
+			JSON[key] = element;
 		}
-		delete JSON.manual
-		delete JSON.editorFields
-		return JSON
+		delete JSON.manual;
+		delete JSON.editorFields;
+		return JSON;
 	}
 
-
 	fromObject(value) {
-		Object.assign(this, value)
-		return toObject()
+		Object.assign(this, value);
+		return toObject();
 	}
 }
 
 class DataTypeClass extends CommonClass {
 	constructor(options = {}) {
-		super(...arguments)
-		this.name = ''
-		this.subName = ''
-		this.preview = ''
-		Object.assign(this, options)
-		this.name = this.name.toLowerCase()
-		this.subName = this.subName.toLowerCase()
-		this.preview = this.preview.toLowerCase()
-
+		super(...arguments);
+		this.name = "";
+		this.subName = "";
+		this.preview = "";
+		Object.assign(this, options);
+		this.name = this.name.toLowerCase();
+		this.subName = this.subName.toLowerCase();
+		this.preview = this.preview.toLowerCase();
 	}
 
-
 	preRender() {
-		super.render()
+		super.render();
 	}
 }
 
 class DescriptionClass extends CommonClass {
 	constructor() {
-		super(...arguments)
-		this.body = ''
+		super(...arguments);
+		this.body = "";
 	}
 
-
 	preRender() {
-		super.render()
+		super.render();
 	}
 }
 
 class VarClass extends CommonClass {
 	constructor(options = {}) {
-		super(...arguments)
-		this.name = null
-		this.value = ''
-		this.type = dataTypes.string
-		Object.assign(this, options)
+		super(...arguments);
+		this.name = null;
+		this.value = "";
+		this.type = dataTypes.string;
+		Object.assign(this, options);
 	}
 }
 
 class EditorFieldsClass {
 	constructor(options) {
-		this.name = ''
-		this.type = 'text'
-		this.dataSet = []
-		this.callback = false
-		this.id = getRandomString()
-		this.input = false
-		Object.assign(this, options)
+		this.name = "";
+		this.type = "text";
+		this.dataSet = [];
+		this.callback = false;
+		this.id = getRandomString();
+		this.input = false;
+		Object.assign(this, options);
 	}
 
-
-	render(parent, value = '') {
-		if(this.input === false) {
-			switch( this.type ) {
+	render(parent, value = "") {
+		if (this.input === false) {
+			switch (this.type) {
 				default:
-					this.input = $(`<input name="${this.name}" id="${this.id}" type="${this.type}" value="${value}">`).appendTo(parent)
-					if(this.dataSet.length > 0) {
+					this.input = $(`<input name="${this.name}" id="${this.id}" type="${this.type}" value="${value}">`).appendTo(parent);
+					if (this.dataSet.length > 0) {
 						this.input.autocomplete({
 							source: this.dataSet.toArray(),
-							minLength: 0
-						})
+							minLength: 0,
+						});
 					}
-					if(this.callback !== false) {
-						this.input.on('change', this.callback)
+					if (this.callback !== false) {
+						this.input.on("change", this.callback);
 					}
 
-					break
+					break;
 			}
 		} else {
-			this.input.appendTo(parent)
-			this.input.val(value)
+			this.input.appendTo(parent);
+			this.input.val(value);
 		}
-		return this.input
+		return this.input;
 	}
 }
---
+++ core/js/layout.class.js
@@ -0,0 +1,31 @@
+class LayoutEditorClass {
+	constructor() {
+		this.block = $("#editor");
+	}
+	/**
+	 * @param  {ManualClass} manual
+	 */
+	render(elem = {}) {
+		this.block.html("");
+		this.elem = elem;
+		if (elem instanceof CommonClass) {
+			elem.editorRender(this.block);
+		}
+	}
+}
+class LayoutTreeClass {
+	constructor() {
+		this.block = $("#tree");
+	}
+	/**
+	 * @param  {ManualClass} manual
+	 */
+	render() {
+		// this.block.html("");
+		this.create_manual = $(`<button type="button" class="btn btn-primary" id="create_manual"></button>`).text('create manual').appendTo(this.block);
+	}
+}
+var layout = {};
+layout.editor = new LayoutEditorClass();
+layout.tree = new LayoutTreeClass();
+layout.tree.render();
