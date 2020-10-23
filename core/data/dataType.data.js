0c748f12116777e073ce4d8e77cec13da1e7c79b
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-10-23 20:08:36 +0300

    1

--- core/data/dataType.data.js
+++ core/data/dataType.data.js
@@ -1,26 +1,26 @@
 dataTypes.toArray = (arr = {}) => {
-	for(let element in dataTypes) {
+	for (let element in dataTypes) {
 		let e = dataTypes[element]
-		if(e instanceof CLASSES.DataTypeClass) {
-			arr[element] = (`${locale._(e.subName)} <small class="dataSet">(${e.name})</small>`)
+		if (e instanceof CLASSES.DataTypeClass) {
+			if (e.subName == e.name) {
+				arr[element] = (`${locale._(e.subName)}`)
+			} else {
+				arr[element] = (`${locale._(e.subName)} (${e.name})`)
+			}
 		}
 	}
 	return arr
 }
-dataTypes.mixed     = new CLASSES.DataTypeClass({name: 'mixed'   , subName: 'mixed'})
-dataTypes.string    = new CLASSES.DataTypeClass({name: 'string'  , subName: 'string'    , preview: 'text'})
-dataTypes.int       = new CLASSES.DataTypeClass({name: 'int'     , subName: 'int'       , preview: '1'})
-dataTypes.boolean   = new CLASSES.DataTypeClass({name: 'boolean' , subName: 'boolean'   , preview: 'true'})
-dataTypes.float     = new CLASSES.DataTypeClass({name: 'float'   , subName: 'float'     , preview: '0.5'})
-dataTypes.object    = new CLASSES.DataTypeClass({name: 'object'  , subName: 'object'    , preview: '{}'})
-dataTypes.array     = new CLASSES.DataTypeClass({name: 'array'   , subName: 'array'     , preview: '[]'})
-dataTypes.timestamp = new CLASSES.DataTypeClass({name: 'int'  	 , subName: 'timestamp' , preview: '1601647148'})
-dataTypes.json      = new CLASSES.DataTypeClass({name: 'string'  , subName: 'json'      , preview: '{a1:0}'})
-dataTypes.img       = new CLASSES.DataTypeClass({name: 'string'  , subName: 'img'       , preview: 'https://placehold.it/30x30'})
-dataTypes.url       = new CLASSES.DataTypeClass({name: 'string'  , subName: 'url'       , preview: 'https://domain'})
-dataTypes.resource  = new CLASSES.DataTypeClass({name: 'resource', subName: 'resource'})
-dataTypes.xml = new CLASSES.DataTypeClass({
-	name: 'string',
-	subName: 'xml',
-	preview: '<element item="">content</element>'
-})
+dataTypes.mixed = new CLASSES.DataTypeClass({ name: 'mixed', subName: 'mixed' })
+dataTypes.string = new CLASSES.DataTypeClass({ name: 'string', subName: 'string', preview: 'text' })
+dataTypes.int = new CLASSES.DataTypeClass({ name: 'int', subName: 'int', preview: '1' })
+dataTypes.boolean = new CLASSES.DataTypeClass({ name: 'boolean', subName: 'boolean', preview: 'true' })
+dataTypes.float = new CLASSES.DataTypeClass({ name: 'float', subName: 'float', preview: '0.5' })
+dataTypes.object = new CLASSES.DataTypeClass({ name: 'object', subName: 'object', preview: '{}' })
+dataTypes.array = new CLASSES.DataTypeClass({ name: 'array', subName: 'array', preview: '[]' })
+dataTypes.timestamp = new CLASSES.DataTypeClass({ name: 'int', subName: 'timestamp', preview: '1601647148' })
+dataTypes.json = new CLASSES.DataTypeClass({ name: 'string', subName: 'json', preview: '{a1:0}' })
+dataTypes.img = new CLASSES.DataTypeClass({ name: 'string', subName: 'img', preview: 'https://placehold.it/30x30' })
+dataTypes.url = new CLASSES.DataTypeClass({ name: 'string', subName: 'url', preview: 'https://domain' })
+dataTypes.resource = new CLASSES.DataTypeClass({ name: 'resource', subName: 'resource' })
+dataTypes.xml = new CLASSES.DataTypeClass({	name: 'string',	subName: 'xml',	preview: '<element item="">content</element>'})
